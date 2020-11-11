LineageOS/android_packages_apps_Dialer

Add back in-call vibration features
Change-Id: Ia30a3c46e9c37d7d73288ec605af8efc3a46a323

src/com/android/dialer/settings/SoundSettingsFragment.java
@@ -102,7 +102,11 @@ public class SoundSettingsFragment extends PreferenceFragment
         if (hasVibrator()) {
             mVibrateWhenRinging.setOnPreferenceChangeListener(this);
         } else {
-            getPreferenceScreen().removePreference(mVibrateWhenRinging);
+            PreferenceScreen ps = getPreferenceScreen();
+            Preference inCallVibration = findPreference(
+                    context.getString(R.string.incall_vibration_category_key));
+            ps.removePreference(mVibrateWhenRinging);
+            ps.removePreference(inCallVibration);
             mVibrateWhenRinging = null;
         }
 

Octavi-OS/platform_packages_apps_Dialer

java/com/android/dialer/app/settings/SoundSettingsFragment.java
@@ -96,7 +96,11 @@ public class SoundSettingsFragment extends PreferenceFragment
     if (hasVibrator()) {
       vibrateWhenRinging.setOnPreferenceChangeListener(this);
     } else {
-      getPreferenceScreen().removePreference(vibrateWhenRinging);
+      PreferenceScreen ps = getPreferenceScreen();
+      Preference inCallVibration = findPreference(
+          context.getString(R.string.incall_vibration_category_key));
+      ps.removePreference(vibrateWhenRinging);
+      ps.removePreference(inCallVibration);
       vibrateWhenRinging = null;
     }
 